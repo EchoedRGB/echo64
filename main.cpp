3d2647fc0863a04841e01e0e71a46c173bf2df19
EchoedRGB <qEchoed@protonmail.ch>
2024-10-30 15:41:24 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "screen.cpp" // Include your new file
+
+int main() {
+    startScreen();
+
+    // Example coordinates
+    //addCoordinate(4, 5); // Add a coordinate as an example
+    //addCoordinate(2, 3); // You can add more coordinates for testing
+    //addCoordinate(63, 63);
+
+    run(); // This will handle the main event loop
+
+    return 0;
+}
